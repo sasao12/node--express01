00d7284df50fab80740202d01b4ad854914518d7
sasao12 <166688575+sasao12@users.noreply.github.com>
2024-05-16 07:01:36 +0000

    Express.js完全入門のコードを追加

--- index.js
+++ index.js
@@ -2,9 +2,45 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+/*
 app.get('/', (req, res) => {
   res.send('Hello World!ただのスタートじゃないぜ...今日は荒天')
+  //res.send('Hello World!ただのスタートじゃないぜ...今日は晴天')
 })
+*/
+
+
+// GETリクエスト
+app.get('/', (req, res) => {
+  res.send({
+    msg:'GET request'
+  });
+});
+
+// POSTリクエスト
+app.post('/', (req, res) => {
+  res.send({
+    msg:'POST request'
+  });
+});
+
+// PUTリクエスト
+app.put('/:id', (req, res) => {
+  res.send({
+    id: req.params.id,
+    msg:'PUT request'
+  });
+});
+
+// DELETEリクエスト
+app.delete('/:id', (req, res) => {
+  res.send({
+    id: req.params.id,
+    msg:'DELETE request'
+  });
+});
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
