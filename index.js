6f1f56c5444098e408dd9a8e1681856273723e96
sasao12 <166688575+sasao12@users.noreply.github.com>
2024-05-16 07:12:35 +0000

    課題に対する呪詛を表示できるようにした

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path');
 
 /*
 app.get('/', (req, res) => {
@@ -8,6 +9,14 @@ app.get('/', (req, res) => {
   //res.send('Hello World!ただのスタートじゃないぜ...今日は晴天')
 })
 */
+// 静的ファイルの提供とキャッシュの有効化
+app.use(express.static('public', { maxAge: 86400000 }));
+
+// test-imageリクエスト
+app.get('/test-image', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/images/test.png'));
+});
+
 
 
 // GETリクエスト
